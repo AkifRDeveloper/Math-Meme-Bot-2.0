404111999e2ab4284f8cb1cda4109a52e8b665ee
AkifRDeveloper <legend.soccer10101@gmail.com>
2026-04-26 09:19:14 -0700

    Remove bot token and prompt for user input
    Removed the bot token from the run command and added a comment to prompt for user input.

---
+++ randomthings.py
@@ -0,0 +1,79 @@
+import requests
+import os
+import random
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def subtract(ctx, left: int, right: int):
+    """subtracts two numbers together."""
+    await ctx.send(left - right)
+
+@bot.command()
+async def multiply(ctx, left: int, right: int):
+    """multiplies two numbers together."""
+    await ctx.send(left * right)
+
+@bot.command()
+async def divide(ctx, left: int, right: int):
+    """divides two numbers together."""
+    await ctx.send(left / right)
+
+@bot.command()
+async def power(ctx, left: int, right: int):
+    """power two numbers together."""
+    await ctx.send(left ** right)
+
+@bot.command()
+async def meme(ctx):
+    files = os.listdir('images')
+    selected = random.choice(files)    
+    with open(f'images/{selected}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("")
+
+
+#put your token for discord in line 76
